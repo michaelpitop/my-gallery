84d8d9a7e727a750378e56c531deb264ab25ad37
michaelpitop <michaelpitop@outlook.com>
2023-10-24 13:51:31 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <div>
-    <h1>michaelpitops</h1>
+    <h1>michaelpitops gallery</h1>
     <p>Welcome to my personal gallery, where I post my drawings, which are of the  geometric contemporary style </p>
-    <form action="text">
-        <input type="text" id="fname" name="fname"><br><br>
-    </form>
 </div>
